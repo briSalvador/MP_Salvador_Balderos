80178258cd5b014e44a74c51ecc1d2b9c09c03d0
briSalvador <salvadorbriannab@gmail.com>
2024-12-01 00:56:30 +0800

    modified input code

--- MP_Salvador_Balderos/main.c
+++ MP_Salvador_Balderos/main.c
@@ -3,11 +3,7 @@
 #include <windows.h>
 #include <stdint.h>
 
-// extern void testFun();
-
 int main(){
-    //testFun();
-
     //INITIALIZE VALUES
     int height = 0;
     int width = 0;
@@ -15,23 +11,52 @@ int main(){
     //GET INPUT
     printf("Instructions: Enter your input separating them with a space after each number.\n");
     printf("Enter your input: ");
-    scanf_s("%d", &height);
-    scanf_s("%d", &width);    
+    scanf_s("%d %d", &height, &width);
 
     int arrSize = height * width;
-    int* arr = (int*)malloc(arrSize * sizeof(int));
+    int** arr = (int**)malloc(height * sizeof(int*));
+    //int* arr = (int*)malloc(arrSize * sizeof(int));
 
-    for (int i = 0; i < arrSize; i++) {
-        scanf_s("%d", &arr[i]);
+    // Allocate memory for each row
+    for (int i = 0; i < height; i++) {
+        arr[i] = (int*)malloc(width * sizeof(int));
     }
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            scanf_s("%d", &arr[i][j]); // Read integers separated by spaces
+        }
+    }
+
+    /* FOR TESTING
+    printf("Height: %d\n", height);
+    printf("Width: %d\n", width);
+    printf("2D Array:\n");
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    */
+
+    // Free allocated memory
+    for (int i = 0; i < height; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    /*for (int i = 0; i < arrSize; i++) {
+        scanf_s("%d", &arr[i]);
+    }*/
 
     //FOR TESTING VALUES ENTERED ONLY, UNCOMMENT AS NEEDED
     //printf("\n height: %d", height);
     //printf("\n width: %d", width);
-    printf("You entered: ");
+    /*printf("You entered: ");
     for (int i = 0; i < arrSize; i++) {
         printf("%d ", arr[i]);
-    }
+    }*/
 
     return 0;
 }
--- MP_Salvador_Balderos/test.asm
+++ MP_Salvador_Balderos/test.asm
@@ -14,3 +14,5 @@ testFun:
     call printf
     add rsp, 8*5
     ret
+
+
