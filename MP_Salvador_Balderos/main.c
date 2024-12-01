df3918d3d4e63dd7b9333a6b3481f3683fda4da9
ernestbalderosa15 <sofia_balderosa@dlsu.edu.ph>
2024-12-01 17:21:47 +0800

    added time check functionality
    The time check only works on 10*10, 100*100 and 1000*1000 arrays per the instructions

--- MP_Salvador_Balderos/main.c
+++ MP_Salvador_Balderos/main.c
@@ -2,18 +2,31 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <stdint.h>
+#include <time.h>
 
 extern double imgCvtGrayIntToDouble(uint8_t x);
 
-int main(){
+int main() {
     //INITIALIZE VALUES
     int height = 0;
     int width = 0;
+    int toTime = 0;
+    int heightWidth = 0;
+    double timeRecord[30];
+    double averageTime;
+    double totalTime = 0;
+    LARGE_INTEGER start, end, frequency;
 
     //GET INPUT
     printf("Instructions: Enter your input separating them with a space after each number.\n");
     printf("Enter your input: ");
     scanf_s("%d %d", &height, &width);
+
+    heightWidth = height * width;
+
+    if (heightWidth == 100 || heightWidth == 10000 || heightWidth == 1000000) {
+        toTime = 1;
+    }
 
     int arrSize = height * width;
     uint8_t** arr = (uint8_t**)malloc(height * sizeof(uint8_t*));
@@ -35,11 +48,35 @@ int main(){
         ans[i] = (double*)malloc(width * sizeof(double));
     }
     
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            ans[i][j] = imgCvtGrayIntToDouble(arr[i][j]);
+    if (toTime == 1) {
+        QueryPerformanceFrequency(&frequency);
+        for (int x = 0; x < 30; x++) {
+            QueryPerformanceCounter(&start);
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    ans[i][j] = imgCvtGrayIntToDouble(arr[i][j]);
+                }
+            }
+
+            QueryPerformanceCounter(&end);
+            totalTime += (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
+            timeRecord[x] = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
+        }
+
+        averageTime = totalTime / (double) 30;
+
+        printf("\nTime taken to execute: %lf ms\n", averageTime);
+
+    }
+    else {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                ans[i][j] = imgCvtGrayIntToDouble(arr[i][j]);
+            }
         }
     }
+    
 
     // FOR TESTING
     printf("\n");
@@ -63,6 +100,23 @@ int main(){
         free(ans[i]);
     }
     free(ans);
+
+    
+    // For Checking
+    if (toTime == 1) {
+        
+        double timeRecordSum = 0; 
+        
+        printf("\nExecution Time log: ");
+        printf("The average time was taken by running the program 30 times.\n");
+        for (int i = 0; i < 30; i++) {
+            printf("(%d): %lf ms \n", i + 1, timeRecord[i]);
+            timeRecordSum += timeRecord[i]; 
+        }
+
+        //PURELY FOR CHECKING
+        printf("\nsum1: %lf; sum2: %lf", totalTime, timeRecordSum);
+    }
 
     /*for (int i = 0; i < arrSize; i++) {
         scanf_s("%d", &arr[i]);
